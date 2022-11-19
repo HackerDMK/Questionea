07c605a04ac6733b4a3121910d5eea31b7c635f6
HackerDMK <hackerdmk@gmail.com>
2022-11-20 02:55:59 +0530

    Fix

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var login= document.getElementById("login");
+var button= document.getElementById("signup");
+
+login.onclick = function(){<a href="loginpage.html"></a>};
+
+button.addEventListener("click", clicked);
